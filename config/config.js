b35ac429d6dd9f3d81e23dd0636ff1404025fe92
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-12-10 23:30:39 +0100

    test

--- config/config.js
+++ config/config.js
@@ -2,5 +2,6 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "https://localhost:7257/api/"
-  : "https://webapiapi123.azure-api.net/api/";
+  : "https://localhost:7257/api/";
 //https://localhost:7257/api/
+// "https://webapiapi123.azure-api.net/api/";
