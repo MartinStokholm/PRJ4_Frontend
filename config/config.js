276b48c60fc30e0d0d55290b262e8fb70f99bbe7
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-12-10 20:18:50 +0100

    DINGDONG

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "https://localhost:7257/api/"
-  : "https://webapi20221210195228.azurewebsites.net/api/";
+  : "https://webapiapi123.azure-api.net/api/";
 //https://localhost:7257/api/
