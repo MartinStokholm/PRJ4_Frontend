f95a6380768d292df46108d8a87badfdd7a2069c
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-12-12 12:12:53 +0100

    fatt

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "https://localhost:7257/api/"
-  : "https://localhost:7257/api/";
+  : "https://fatt-api.azurewebsites.net";
 //https://localhost:7257/api/
 // "https://webapiapi123.azure-api.net/api/";
